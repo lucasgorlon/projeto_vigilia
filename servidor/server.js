f067525d9d4071357313d30951074aee947b354f
lucasgorlon <sgorlon.ribeiro@gmail.com>
2026-02-03 15:11:00 -0400

    atualizando o token

--- servidor/server.js
+++ servidor/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const PUSH_TOKEN = 'ExponentPushToken[YmMDtmLpqzsuhVoUNGc_pT]';
+const PUSH_TOKEN = 'ExponentPushToken[7qHbvaGu8OR8iHxBlur57v]';
 
 // --- ROTAS ---
 
